442c3df30aecb0405807705321da24f88598cd4b
garantae <garantae@terpmail.umd.edu>
2026-05-10 13:47:08 -0400

    updates

--- index.js
+++ index.js
@@ -5,41 +5,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-// //main
-// "use strict";
-// const express = require("express");
-// const app = express();
-// const path = require("path");
-// let portNumber = process.argv[2];
-// const bodyParser = require("body-parser");
-// require("dotenv").config({
-//    path: path.resolve(__dirname, "credentialsDontPost/.env"),
-// });
-// const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config({
+   path: path.resolve(__dirname, "credentials/.env"),
+   quiet: true,
+});
 
-// app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
+process.stdin.setEncoding("utf8");
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"));
+app.use(bodyParser.urlencoded({extended:false}));
 
-// app.set("view engine", "ejs");
-// app.set("views", path.resolve(__dirname, "templates"));
+const args = process.argv;
+const uri = process.env.MONGO_CONNECTION_STRING;
 
-// //pages home
-// app.get("/", (request, response) => {
-//     response.render("home");
-// });
+// TODO: we'll use mongoose to connect to the DB (b/c connection string has db name), do our thing, and then disconnect
+// use mongoose.model to access collection
 
-// //test
-// app.get("/challenge", (request, response) => {
-//     response.render("typerTest");
-// });
+// Homepage
+app.get("/", (request, response) => {
+    response.render("home");
+});
 
-// //score
-// app.get("/getScores", (request, response) => {
-//     response.render("scores");
-// });
+//test
+app.get("/challenge", (request, response) => {
+    response.render("typerTest");
+});
 
-// //score
-// app.post("/getScores", (request, response) => {
-//     var leaderboard = "<add score from mongoDB>";
-//     var variables = {leaderboard: leaderboard};
-//    response.render("leaderboard", variables);
-// });
+//score
+app.get("/getScores", (request, response) => {
+    response.render("scores");
+});
+
+//score
+app.post("/getScores", (request, response) => {
+    var leaderboard = "<add score from mongoDB>";
+    var variables = {leaderboard: leaderboard};
+   response.render("leaderboard", variables);
+});
