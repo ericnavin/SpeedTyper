2a98b8147005199d22209d5e472c44772e2f93ca
Natalie Figueira <nfigueir@terpmail.umd.edu>
2026-05-10 12:42:46 -0400

    basic file creation

---
+++ index.js
@@ -0,0 +1,36 @@
+//main
+"use strict";
+const express = require("express");
+const app = express();
+const path = require("path");
+let portNumber = process.argv[2];
+const bodyParser = require("body-parser");
+require("dotenv").config({
+   path: path.resolve(__dirname, "credentialsDontPost/.env"),
+});
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"));
+
+//pages home
+app.get("/", (request, response) => {
+    response.render("home");
+});
+
+//test
+app.get("/challenge", (request, response) => {
+    response.render("typerTest");
+});
+
+//score
+app.get("/getScores", (request, response) => {
+    response.render("scores");
+});
+
+//score
+app.post("/getScores", (request, response) => {
+   
+});
